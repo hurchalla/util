8139f8b43c84645b387ad68b775889ac232527b3
Jeff Hurchalla <hurchalla@yahoo.com>
2018-06-20 06:35:26 -0700

    Update assert_handler.h

--- assert_handler.h
+++ assert_handler.h
@@ -10,12 +10,7 @@
    
 /* To enable a project containing both C and C++ code to use one single assert
 handler, you can surround these function declarations using the typical
-#ifdef __cplusplus and extern "C" trick.  However, this trick is not a general
-solution for all needs - it restricts what your pbcAssertHandler can do; marking
-these functions as extern C will result in undefined behavior if
-pbcAssertHandler throws, even if all your code is C++. That's fine for mixed
-C/C++ code, since presumably in that case you would never write a handler that
-throws. It just isn't a general solution.
+#ifdef __cplusplus and extern "C" trick.
 */
 void pbcAssertHandler(const char* failedAssertion, const char* filename,
                       int line);
