dec6defec3f19723b8f97e9d14921e6435fbff7a
Jeff Hurchalla <hurchalla@yahoo.com>
2018-06-20 06:34:00 -0700

    Update assert_handler_cpp.cpp

--- assert_handler_cpp.cpp
+++ assert_handler_cpp.cpp
@@ -9,12 +9,8 @@ assert violations. A few possible custom handlers might be:
   - A handler that prints to stderr and calls abort().
   - A handler that prints to stderr and spins/sleeps and wakes periodically to
     look for a debugger or signal.
-  - A handler that wraps assertion info in hurchalla::BrokenContract (or derived
-    class) and throws
 Note that
-  - A handler for a plugin architecture should probably always use (C++)
-    exceptions in order to avoid crashing the host in the event of a plugin 
-    contract violation.
+  - A handler must never throw an exception.
   - If you want to log to file in your handler, it could be useful to print the
     call stack.  Unfortunately this is platform specific and nontrivial; see
     backtrace() and backtrace_symbols() on unix, and possibly
@@ -22,43 +18,31 @@ Note that
     have name mangling, and you'd want to unmangle when logging; see 
     __cxa_demangle() for gcc and perhaps UnDecorateSymbolName() for MSVC.
     Overall, https://oroboro.com/stack-trace-on-crash/ may be useful.
-Some examples follow: */
-#if 0
-#   include <sstream>
-    // If your assert handler throws, the exception must be derived from
-    // hurchalla::BrokenContract
-    void pbcAssertHandler(const char* failedAssertion, const char* filename,
-                          int line)
-    {
-        std::ostringstream ss;  
-        ss << "Assert failed (" << failedAssertion << "): " << filename
-           << " line " << line;
-        throw hurchalla::BrokenContract(ss.str());
-    }
-#else
-#   include <iostream>
-#   include <cstdlib>
-    void pbcAssertHandler(const char* failedAssertion, const char* filename,
-                          int line)
-    {
-        std::cerr << "Assert failed (" << failedAssertion << "): " << filename
-                  << " line " << line << "\n";
-        std::abort();
-        // You could potentially use __debugbreak() for msvc, and
-        // __builtin_trap() for gcc
-    }
-#endif
+An example handler follows: */
+
+#include <iostream>
+#include <cstdlib>
+void pbcAssertHandler(const char* failedAssertion, const char* filename,
+                      int line)
+{
+    std::cerr << "Assert failed (" << failedAssertion << "): " << filename
+              << " line " << line << "\n";
+    std::abort();
+    // You could potentially use __debugbreak() for msvc, and
+    // __builtin_trap() for gcc
+}
 
 /*  You can set your executable's contract assert level by creating a predefined
 macro specific to compiling this file, called:
     PBC_ASSERT_LEVEL
-  If predefined, this macro must be set so that 0 <= PBC_ASSERT_LEVEL <= 3. When
-you set PBC_ASSERT_LEVEL lower than some value X, the contract-asserts
+  If predefined, this macro must be set so that 0 <= PBC_ASSERT_LEVEL <= 3.
+Keeping in mind that the unnumbered asserts have an implicit level number of 1,
+when you set PBC_ASSERT_LEVEL lower than some value X, the contract-asserts
 invariantX(), postconditionX(), assert_bodyX(), etc become effectively disabled
-after linking (with the exception of the level0 static_asserts). If you are
-using link-time-optimization, typically those particular contract-asserts will
-be completely removed from the code.  If you do not predefine PBC_ASSERT_LEVEL,
-it will be defined below in this file with a default value (the default ensures
+after linking (with the exception of the static asserts). If you are using
+link-time-optimization, typically those particular contract-asserts will be
+completely removed from the code.  If you do not predefine PBC_ASSERT_LEVEL, it
+will be defined below in this file with a default value (the default ensures
 asserts are active for debug mode and inactive for release mode).
   You can optionally set your executable's level specifically for precondition
 asserts by predefining a macro called:
@@ -75,7 +59,7 @@ macro to a value such that 0 <= PBC_ASSERT_LEVEL_PRE <= 3.
 Note that if you want settings specific to your particular project/executable,
 it's best to predefine PBC_ASSERT_LEVEL and PBC_ASSERT_LEVEL_PRE rather than
 relying on these defaults. Note also that NDEBUG is used only for determining
-default assertion levels -  - it does not disable precondition asserts when you
+default assertion levels - it does not disable precondition asserts when you
 have predefined PBC_ASSERT_LEVEL_PRE, and does not disable any other asserts
 when you have predefined PBC_ASSERT_LEVEL.
 */
