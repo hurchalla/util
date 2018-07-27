f17809325e496d1eb10e308500aa4b4f4e9a2099
Jeff Hurchalla <hurchalla@yahoo.com>
2018-07-26 17:26:06 -0700

    Update assert_handler.h

--- include/hurchalla/programming_by_contract/assert_handler.h
+++ include/hurchalla/programming_by_contract/assert_handler.h
@@ -1,5 +1,5 @@
-/* --- This file is distributed under the MIT Open Source License,
-   as detailed in "LICENSE.TXT" in this directory --- */
+/* --- This file is distributed under the MIT Open Source License, as detailed
+   in "LICENSE.TXT" in the root of the programming_by_contract repository --- */
 
 #ifndef HURCHALLA_PBC_ASSERT_HANDLER_H_INCLUDED
 #define HURCHALLA_PBC_ASSERT_HANDLER_H_INCLUDED
